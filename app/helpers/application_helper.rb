0a46be26bc260b1936946afb7030db2ba5505a82
Evgeny Lapin <lev@openteam.ru>
2013-11-06 17:00:49 +0700

    freeze jquery-rails gem, update stop_ie gem

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def entry_date
+    @entry_date ||= begin
+                      @page.regions.to_hash.each do |region_name, region|
+                        if (since = region.try(:[], 'content').try(:[], 'since'))
+                          return since
+                        end
+                      end
+                      nil
+                    end
+  end
+
 end
