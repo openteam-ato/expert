896465a4801173a45c22d85335e7379d95970df0
Evgeny Lapin <lev@openteam.ru>
2013-11-07 10:25:54 +0700

    styles for governor message on main page + news list region

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,14 @@ module ApplicationHelper
                     end
   end
 
+  def render_partial_for_region(region, prefix = nil)
+    if region && (region.response_status == 200 || !region.response_status?)
+      partial = prefix.blank? ? "regions/#{region.template}" : "regions/#{prefix}_#{region.template}"
+      render :partial => partial,
+      :locals => { :object => region.content, :response_status => region.response_status }
+    else
+      render :partial => 'regions/error', :locals => { :region => region }
+    end
+  end
+
 end
