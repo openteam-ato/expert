89661bb239123bc406f305317a61054cfe36a836
Evgeny Lapin <lev@openteam.ru>
2013-11-12 17:24:02 +0700

    add sitemap with styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,6 +21,18 @@ module ApplicationHelper
     end
   end
 
+  def resized_image_url(url, width, height, options = { :crop => '!', :magnify => 'm', :orientation => 'n' })
+    return if url.blank?
+    if url.match(/\d+\/region\/\d+/)
+      return url.gsub(/(\/files\/\d+\/region\/(\d+|\/){8})/) { "#{$1}#{width}-#{height}/" }
+    end
+    if url.match(/\/files\/\d+\/\d+-\d+\//)
+      image_url, image_id, image_width, image_height, image_crop, image_filename = url.match(%r{(.*)/files/(\d+)/(?:(\d+)-(\d+)(\!)?/)?(.*)})[1..-1]
+      return "#{image_url}/files/#{image_id}/#{width}-#{height}#{options[:crop]}#{options[:magnify]}#{options[:orientation]}/#{image_filename}"
+    end
+    url
+  end
+
   def archive_links(parts_array)
     parts_array = parts_array.compact.select { |part| part.content.items }
 
@@ -70,6 +82,17 @@ module ApplicationHelper
   def extension(filename)
     return if filename.blank?
     filename.match(/\.(\w+)$/).try(:[], 1).downcase
+  end
+
+  def render_navigation(hash)
+    return '' if hash.nil? || hash.empty?
+    content_tag :ul do
+      hash.map do |key, value|
+        content_tag :li, :class => value['selected'] ? 'selected' : nil do
+          link_to(value['title'], value['path'])+render_navigation(value['children'])
+        end
+      end.join.html_safe
+    end
   end
 
 end
